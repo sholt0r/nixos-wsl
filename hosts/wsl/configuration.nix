b46e2501b21e55e9764cbaf0ba3283a98abb9705
sholt0r <sholtor@kobu.au>
2024-09-24 13:16:23 +0000

    Changes to flake

--- hosts/wsl/configuration.nix
+++ hosts/wsl/configuration.nix
@@ -5,7 +5,7 @@
 # NixOS-WSL specific options are documented on the NixOS-WSL repository:
 # https://github.com/nix-community/NixOS-WSL
 
-{ pkgs, inputs, home-manager, ... }:
+{ pkgs, inputs, ... }:
 
 {
   imports = [
